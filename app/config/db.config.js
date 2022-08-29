7c606edeebb6d0a50be480f5bb66c9e7423ecbf0
Natalia <nataliamakarchuk17@gmail.com>
2022-08-29 18:33:59 +0300

    set db identifier

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: process.env.DB_ENV === 'development' ? 'localhost' : process.env.DB_HOST,
   USER: "nataliamakarchuk",
   PASSWORD: "password",
-  DB: "api",
+  DB: process.env.DB_ENV === 'development' ? 'api' : process.env.DB_IDENTIFIER,
   dialect: "postgres",
   port: 5432,
   pool: {
