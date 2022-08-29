4549c7fedc6581242b19bc0a8964e376eafc40f1
Natalia <nataliamakarchuk17@gmail.com>
2022-08-29 18:29:43 +0300

    update db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  HOST: "localhost",
+  HOST: process.env.DB_ENV === 'development' ? 'localhost' : process.env.DB_HOST,
   USER: "nataliamakarchuk",
   PASSWORD: "password",
   DB: "api",
   dialect: "postgres",
+  port: 5432,
   pool: {
     max: 5,
     min: 0,
