3f7412a606db8e3ddfdcfa094ee81a4bd26025ab
Natalia <nataliamakarchuk17@gmail.com>
2022-09-16 10:29:52 +0300

    add socket io

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const http = require("http");
+const Server = require('socket.io');
 const app = express();
+// const httpServer = new http.Server(app);
+// const io = new Server(httpServer, { cors: { origin: '*' } });
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST", "PUT"]
+  }
+});
 
 const PORT = process.env.API_PORT || 3000;
 
@@ -24,10 +35,20 @@ app.use(cookieParser());
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+io.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`)
 })
+
+// app.listen(PORT, () => {
+//   console.log(`App running on port ${PORT}.`)
+// })
 
 // run only if need to recreete users table
 // const db = require("./app/models");
