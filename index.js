0044a642fdbdd66dca37a7b708c75c2f4ed7b2c1
Natalia <nataliamakarchuk17@gmail.com>
2022-11-12 12:39:17 +0200

    update cors with new client url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = require("http").createServer(app);
 const ioSocket = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:4200", "http://localhost", "https://natali0073.github.io"],
+    origin: ["http://localhost:4200", "http://localhost", "http://chatapi-env.eba-594gvkfb.eu-west-2.elasticbeanstalk.com"],
     credentials: true
   }
 });
@@ -16,7 +16,7 @@ exports.io = ioSocket;
 const PORT = process.env.API_PORT || 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:4200", "http://localhost:80", "https://natali0073.github.io"],
+  origin: ["http://localhost:4200", "http://localhost:80", "http://chatapi-env.eba-594gvkfb.eu-west-2.elasticbeanstalk.com"],
 };
 
 app.use(cors(corsOptions));
